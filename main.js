037ec32cd738fab4f3f80899df065f3d91cb64bf
JFJoao <jfjoaojf@hotmail.com>
2023-09-01 16:23:56 -0300

    tho the mountains

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { catalogo } from "./src/utilidades.js";
 
+// Construir catalogo pagina principal
 for (const produtoCatalogo of catalogo) {
   const cartaoProduto = `
     <div class="card-produto">
@@ -11,26 +12,57 @@ for (const produtoCatalogo of catalogo) {
         <p class="marcaProduto">${produtoCatalogo.marca}</p>
         <p>${produtoCatalogo.nome}</p>
         <p>$${produtoCatalogo.preco}</p>
-        <button class="botaoAdicionar">Adicionar</button>
+        <button class="botaoAdicionar-${produtoCatalogo.id}">Adicionar</button>
     </div>`;
 
     document.getElementById("container-produto").innerHTML += cartaoProduto;
 }
 
+// Abrir e fechar carrinho
 const abrirCarrinhoButton = document.getElementById('abrirCarrinho');
 const carrinhoArea = document.querySelector('.carrinhoArea');
 const fecharCarrinhoButton = document.getElementById('fecharCarrinho');
-
+// abrir carrinho no icone
 abrirCarrinhoButton.addEventListener('click', () => {
   carrinhoArea.classList.toggle('aberto');
 });
-
-document.addEventListener('click', (event) => {
-  if (!carrinhoArea.contains(event.target) && !abrirCarrinhoButton.contains(event.target)) {
-    carrinhoArea.classList.remove('aberto');
-  }
-}); 
-
+// Fechar carrinho ao clicar fora
+// document.addEventListener('click', (event) => {
+//   if (!carrinhoArea.contains(event.target) && !abrirCarrinhoButton.contains(event.target)) {
+//     carrinhoArea.classList.remove('aberto');
+//   }
+// }); 
+// fechar carrinho no icone x
 fecharCarrinhoButton.addEventListener('click', () => {
   carrinhoArea.classList.remove('aberto');
 });
+
+function adicionarAoCarrinho(idProduto) {
+const produto = catalogo.find((p) => p.id === idProduto);
+const containerProdutosCarrinho = document.getElementById("produtosCarrinho");
+const cartaoProdutoCarrinho = `<article>
+    <button id="fechar-carrinho"">
+      <i></i>
+    </button>
+    <img
+      src="./assets/img/${produto.imagem}"
+      alt="Carrinho: ${produto.nome}"
+      />
+    <div>
+      <p>
+        ${produto.nome}
+      </p>
+      <p>Tamanho: M</p>
+      <p>$${produto.preco}</p>
+    </div>
+  </article>`;
+  containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+// containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+
+
+for (const produtoCatalogo of catalogo) {
+  document
+    .getElementById(`adicionar-${produtoCatalogo.id}`)
+    .addEventListener("click", () => adicionarAoCarrinho(produtoCatalogo.id));
+}
+}
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -22,23 +22,20 @@ export function adicionarAoCarrinho(idProduto) {
   const produto = catalogo.find((p) => p.id === idProduto);
   const containerProdutosCarrinho =
     document.getElementById("produtos-carrinho");
-  const cartaoProdutoCarrinho = `<article class="flex bg-slate-100 rounded-lg p-1 relative">
-    <button id="fechar-carrinho" class="absolute top-0 right-2">
-      <i
-        class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"
-      ></i>
+  const cartaoProdutoCarrinho = `<article>
+    <button id="fechar-carrinho"">
+      <i></i>
     </button>
     <img
       src="./assets/img/${produto.imagem}"
       alt="Carrinho: ${produto.nome}"
-      class="h-24 rounded-lg"
-    />
-    <div class="py-2">
-      <p class="text-slate-900 text-sm">
+      />
+    <div>
+      <p>
         ${produto.nome}
       </p>
-      <p class="text-slate-400 text-xs">Tamanho: M</p>
-      <p class="text-green-700 text-lg">$${produto.preco}</p>
+      <p>Tamanho: M</p>
+      <p>$${produto.preco}</p>
     </div>
   </article>`;
   containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
